7d479ec98b9f327d0ce9ff54ffe5194f6fd3b8dc
khalidhussain-dev <khalidhussainchandio9@gmail.com>
2025-03-25 10:19:12 +0500

    add baseline HTML and CSS design for Countdown & light switch

--- mid-exam/src/component/CountDownSwitchLight.jsx
+++ mid-exam/src/component/CountDownSwitchLight.jsx
@@ -6,54 +6,68 @@ import './CountDownSwitchLight.css'
 function CountDownSwitchLight() {
 
 
-    const [theme, setTheme] = useState(true);
+    const [theme, setTheme] = useState(false);
     const [timer, setTimer] = useState(30);
+    const [modemsg, setmodemsg] = useState('Night Mode')
 
     function changeTheme() {
         setTheme(!theme);
+        if (theme) {
+            setmodemsg('Night Mode')
+        }
+        else {
+            setmodemsg('Light Mode')
+        }
+
     }
 
     function timerTick() {
 
-        setTimeout(() => {
-
+        setInterval(() => {
+            setTimer(timer - 1);
             if (timer == 0) {
-                exit;
+                return;
             }
-
-            setTimer(timer - 1)
 
         }, 1000)
 
     }
 
     function timerStart() {
+        if (!theme) {
+            return
+        }
+
+        else {
+            timerTick();
+
+        }
 
     }
 
 
     return (
 
-        <div className="container" >
+        <div className="container" style={{ background: theme ? 'white' : 'black', color: theme ? 'black' : 'white' }} >
             <div className="header">
                 <h1>Countdown & Light Switch</h1>
                 <div className="toggle-container">
                     <label className="toggle-switch">
-                        <input typeName="checkbox" id="themeToggle" />
+                        <input type="checkbox" id="themeToggle" onClick={changeTheme} />
                         <span className="slider"></span>
                     </label>
-                    <span>Light Mode</span>
+                    <span>{modemsg}</span>
                 </div>
             </div>
 
 
             <div className="timer-section">
                 <div className="progress-bar">
-                    <div className="progress" id="progress"></div>
+                    <div className="progress" id="progress" style={{}} ></div>
                 </div>
                 <div className="timer" id="timerDisplay">{timer}s</div>
                 <div className="btn-group">
-                    <button id="startButton"  >Start Timer</button>
+                    <button id="startButton" onClick={timerStart} >Start Timer</button>
                     <button id="resetButton" >Reset Timer</button>
                 </div>
                 <div className="message" id="messageArea"></div>
