f3c00c7fb1e9ae406134c9c3fb51c54947e83836
khalidhussain-dev <khalidhussainchandio9@gmail.com>
2025-03-25 09:56:53 +0500

    add baseline HTML and CSS design for Countdown & light switch

--- mid-exam/src/component/CountDownSwitchLight.jsx
+++ mid-exam/src/component/CountDownSwitchLight.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CountDownSwitchLight.css'
+
 
 
 function CountDownSwitchLight() {
 
+
+    const [theme, setTheme] = useState(true);
+    const [timer, setTimer] = useState(30);
+
+    function changeTheme() {
+        setTheme(!theme);
+    }
+
+    function timerTick() {
+
+        setTimeout(() => {
+
+            if (timer == 0) {
+                exit;
+            }
+
+            setTimer(timer - 1)
+
+        }, 1000)
+
+    }
+
+    function timerStart() {
+
+    }
+
+
     return (
+
         <div className="container" >
             <div className="header">
                 <h1>Countdown & Light Switch</h1>
@@ -22,10 +51,10 @@ function CountDownSwitchLight() {
                 <div className="progress-bar">
                     <div className="progress" id="progress"></div>
                 </div>
-                <div className="timer" id="timerDisplay">30s</div>
+                <div className="timer" id="timerDisplay">{timer}s</div>
                 <div className="btn-group">
-                    <button id="startButton">Start Timer</button>
-                    <button id="resetButton" style="display: none;">Reset Timer</button>
+                    <button id="startButton"  >Start Timer</button>
+                    <button id="resetButton" >Reset Timer</button>
                 </div>
                 <div className="message" id="messageArea"></div>
             </div>
